2a7196e20e9668721f96fd5b06ebbba2b4eaaba7
Merit <Merit Eller@DESKTOP-IHQH10C.(none)>
2018-06-25 12:23:19 +0300

    first development

--- src/App.js
+++ src/App.js
@@ -1,21 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { Card, Icon, Image } from 'semantic-ui-react';
+import WeatherIcon from 'react-icons-weather';
+
+
+
+const WeatherComponent = (props) => {
+  return (
+    <div className="icon">
+      <WeatherIcon name="owm" iconId={props.iconId} flip="horizontal" rotate="90" />
+    </div>
+  )
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      forecasts: [
+        { day: "Mon", iconId: "200", maxDegree: "71", minDegree: "61" },
+        { day: "Tue", iconId: "300", maxDegree: "71", minDegree: "61" },
+        { day: "Wed", iconId: "800", maxDegree: "71", minDegree: "61" },
+        { day: "Thu", iconId: "500", maxDegree: "71", minDegree: "61" },
+        { day: "Fri", iconId: "600", maxDegree: "71", minDegree: "61" }
+      ]
+    }
+  }
+
+  fiveDayForecast() {
+    return (
+      <div>
+        {this.state.forecasts.map((forecast) => this.renderDailyForecast(forecast))}
+      </div>
+    );
+  }
+
+  renderDailyForecast(forecast) {
+    return (
+      <ForecastCard
+        key={forecast.day}
+        forecast={forecast}
+      />
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">WeatherApp</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          {this.fiveDayForecast()}
+        </div>
       </div>
     );
   }
+}
+
+const ForecastCard = ({ forecast }) => {
+  return (<Card>
+    <Card.Header>{forecast.day}</Card.Header>
+    <Card.Content>
+      <WeatherComponent iconId={forecast.iconId} />
+      <div>
+        <span className="degree">
+          {forecast.minDegree} °C
+      </span>
+        -
+      <span className="degree">
+          {forecast.maxDegree} °C
+      </span>
+      </div>
+    </Card.Content>
+  </Card>
+  );
 }
 
 export default App;
